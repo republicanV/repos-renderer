1acb9f38357ae55e59aabf19e82cbdd650d42273
Vadim Sapovsky <vadym.sapovskyi@wework.com>
2020-10-16 20:38:03 +0300

    Setup default theme

---
+++ src/app/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    main: '#fff',
+  },
+
+  spaces: {
+    default: '1rem',
+  },
+};
+
+export { theme };
