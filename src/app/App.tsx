74017c9e8781c191070296339dc1c4a5832ddceb
Vadim Sapovsky <vadym.sapovskyi@wework.com>
2020-10-16 20:41:55 +0300

    Create App component

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+// External dependencies
+import React, { FC } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import {ThemeProvider} from 'styled-components';
+
+// Local modules
+import client from 'api/apolloClient';
+import { theme } from 'app/theme';
+import LayoutContainer from 'layout/LayoutContainer';
+import 'app/App.css';
+
+const App: FC = () => {
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <LayoutContainer />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
+};
+
+export default App;
